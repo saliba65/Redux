8215d2a0cbfed86baa6b3eb1a51ed472711300bb
saliba65 <lucassaliba10@gmail.com>
2020-07-23 20:16:19 -0300

    Segundo commit

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Video from "./components/Video/Video";
+import { Provider } from "react-redux";
+import store from "./store/index";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Provider store={store}>
+          <Video />
+          <Sidebar />
+        </Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
